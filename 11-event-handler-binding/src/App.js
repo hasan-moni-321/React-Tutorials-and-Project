8bd71739c5c43a258763a013d3997a6459db9860
Hasan Moni <62002387+hasan-moni-321@users.noreply.github.com>
2023-05-23 10:15:27 +0600

    Add files via upload

---
+++ 11-event-handler-binding/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import EVENT_BINDING from "./EVENT_BINDING/index.js" 
+
+export default function App() {
+  return (
+    <div>
+      <EVENT_BINDING/> 
+    </div>
+  )
+}
---
+++ 11-event-handler-binding/src/EVENT_BINDING/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+export default class EVENT_BINDING extends Component {
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         count : 0
+      }
+      this.handleClick = this.handleClick.bind(this) // binding in constructor 
+    }
+
+    handleClick(){
+        this.setState({
+            count : this.state.count + 1
+        })
+    }
+
+    render() {
+        return (
+        <div>
+            <h1>{this.state.count}</h1>
+            <button onClick={this.handleClick}>increase</button>
+        </div>
+        )
+    }
+}
+
+
