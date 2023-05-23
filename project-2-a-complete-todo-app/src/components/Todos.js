09c595f825c67527a9e8b422649122d3e4bfe701
Hasan Moni <62002387+hasan-moni-321@users.noreply.github.com>
2023-05-23 10:30:35 +0600

    Add files via upload

---
+++ project-2-a-complete-todo-app/src/components/Todos.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import Todo from './Todo.js'; 
+import style from './todos.module.css'; 
+
+const Todos = (props) => { 
+
+    
+    return (
+        <section className={style.todos}>
+            {
+                props.todos.map((todo) =>  (
+                <Todo 
+                    todo={todo.todo} 
+                    key={todo.id} 
+                    id={todo.id} 
+                    onRemoveTodo={props.onRemoveTodo} 
+                /> ))  
+            }
+        </section> 
+    )
+}
+
+export default Todos
