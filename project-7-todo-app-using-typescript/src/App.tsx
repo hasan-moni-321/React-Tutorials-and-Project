4018e9e5ea35ad2f93901a0f8e481103600b3ce3
Hasan Moni <62002387+hasan-moni-321@users.noreply.github.com>
2023-05-23 10:36:21 +0600

    Add files via upload

---
+++ project-7-todo-app-using-typescript/src/App.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import Todos from './components/Todos'; 
+import AddTodo from './components/AddTodo';
+import { TodoType } from './types';
+
+ 
+
+
+const todosData = [
+  {
+    id: "1",
+    title: "todo title 1"
+  },
+  {
+    id: "2", 
+    title: 'todo title 2'
+  }, 
+  {
+    id: "3",
+    title: 'todo title 3'
+  }, 
+  {
+    id: "4", 
+    title: 'todo title 4'
+  }, 
+  {
+    id: "5", 
+    title: 'todo title 5'
+  }, 
+  {
+    id: "6", 
+    title: 'todo title 6' 
+  }
+]
+
+const App = () => {
+
+  const [todos, setTodos] = useState(todosData); 
+
+  const handleDeleteTodo = (id: string) => { 
+    const filteredTodos = todos.filter(todo => todo.id!== id); 
+    setTodos(filteredTodos);  
+  }
+
+  const handleAddNewTodo = (newTodo: TodoType) => {
+    setTodos((prevState) => [...prevState, newTodo]);  
+  }
+
+
+  return (
+    <div>
+      <AddTodo handleAddNewTodo={handleAddNewTodo}/> 
+      <Todos todos={todos}
+      handleDeleteTodo={handleDeleteTodo}/> 
+    </div>
+  )
+}
+
+export default App
+
---
+++ project-7-todo-app-using-typescript/src/types.ts
@@ -0,0 +1,19 @@
+
+export type TodoType = {
+    id: string, 
+    title: string 
+}
+
+export type TodosProps = {
+    todos: TodoType[], 
+    handleDeleteTodo: Function 
+}
+
+export type TodoProps = {
+    todo: TodoType; 
+    handleDeleteTodo: Function
+}
+
+export type AddTodoProps = {
+    handleAddNewTodo: Function
+}
